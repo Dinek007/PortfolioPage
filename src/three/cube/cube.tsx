b9d7d453a51ce02d5a681fe5287477e83b38d12e
dinek007 <wilk.dinek@gmail.com>
2023-06-13 12:17:36 +0200

    update skills pic path

--- src/three/cube/cube.tsx
+++ src/three/cube/cube.tsx
@@ -3,7 +3,7 @@ import { BoxGeometry, Mesh, MeshStandardMaterial, TextureLoader } from 'three';
 
 export const Cube: React.FC<{src:string}>  = ({src}) => {
     const texture = new TextureLoader().load(
-      `src/assets/skillsTextures/${src}.png`
+      `../src/assets/skillsTextures/${src}.png`
     );
     const material = new MeshStandardMaterial({map: texture});
     const geometry = new BoxGeometry(1, 1, 1);
