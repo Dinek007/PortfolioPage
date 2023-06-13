a76de08da2956c5733c8a55a911e5241a0505b8c
dinek007 <wilk.dinek@gmail.com>
2023-06-13 12:34:27 +0200

    update cube color

--- src/three/cube/cube.tsx
+++ src/three/cube/cube.tsx
@@ -2,11 +2,16 @@
 import { BoxGeometry, Mesh, MeshStandardMaterial, TextureLoader } from 'three';
 
 export const Cube: React.FC<{src:string}>  = ({src}) => {
-    const texture = new TextureLoader().load(src);
-    const material = new MeshStandardMaterial({map: texture});
-    const geometry = new BoxGeometry(1, 1, 1);
-  
-    return (
-      <mesh material={material} geometry={geometry} position={[0, 0, 0]} scale={3} />
-    );
-  }
+  //const texture = new TextureLoader().load(src);
+  const material = new MeshStandardMaterial({ color: 0xff00ff });
+  const geometry = new BoxGeometry(1, 1, 1);
+
+  return (
+    <mesh
+      material={material}
+      geometry={geometry}
+      position={[0, 0, 0]}
+      scale={3}
+    />
+  );
+}
