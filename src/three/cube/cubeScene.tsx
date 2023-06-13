40d117200c83cfae2295e7fa3d12559a296e24d0
dinek007 <wilk.dinek@gmail.com>
2023-06-13 13:02:36 +0200

    fix mobile models

--- src/three/cube/cubeScene.tsx
+++ src/three/cube/cubeScene.tsx
@@ -3,13 +3,14 @@ import { Canvas } from "@react-three/fiber";
 import { Float } from "@react-three/drei";
 import CustomOrbitControls from "../orbitControls";
 import { Cube } from "./cube";
+import { isMobile } from "react-device-detect";
 
-export const CubeScene: React.FC<{src:string}> = ({src}) => {
+export const CubeScene: React.FC<{ src: string }> = ({ src }) => {
   return (
     <Canvas>
       <ambientLight intensity={0.7} />
       {/* <pointLight position={[0, 0, 2]} intensity={2} /> */}
-      <CustomOrbitControls />
+      {!isMobile ? <CustomOrbitControls /> : <></>}
       {/* <axesHelper scale={3}/> */}
       <Float speed={1} rotationIntensity={2} floatIntensity={1}>
         <Cube src={src} />
